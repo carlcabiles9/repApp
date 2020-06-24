d3e83a3953508bfa17c491649474c86f541094ac
carlcabiles9 <cabiles.carl@yahoo.com>
2020-06-24 21:44:46 +0800

    Rendered views of other controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
     def index; 
-    
+      @projects = Project.all
+      @reports = Report.all
+    end
+    def daily
+      @time = Date.today.to_s(:short)
+      @days = User.find(current_user.id).reports.where(user_id: current_user.id, created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+      @recipients = Recipient.all
+    end
+    def send_now
+      
     end
     private
     def home_params
