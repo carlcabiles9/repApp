c4d0538442cf636ea05137c5d466e7bb6a20d23b
carlcabiles9 <cabiles.carl@yahoo.com>
2020-06-25 19:37:05 +0800

    Added mailers for daily report

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
   default from: 'cccabiles@cdasia.com'
   layout 'mailer'
--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-class UserMailer < ApplicationMailer
+class DailyMailer < ApplicationMailer
   default from: 'cccabiles@cdasia.com'
 
-  def email_report(user, _reports, mail_opts)
-    @try = User.find(user.id).reports.where(created_at: Date.today.beginning_of_week..Date.today.end_of_week)
+  def email_report(user, report, mail_opts)
     @user = user
     @report = report
     @url = 'http://localhost/3000'
