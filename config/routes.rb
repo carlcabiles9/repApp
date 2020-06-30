008139fdf17cfcc2967241c69cb6b4cd82faff7e
carlcabiles9 <cabiles.carl@yahoo.com>
2020-06-30 09:08:59 +0800

    Added route for button to send mail manually

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  
   get 'projects/:id/destroy', to: 'projects#destroy_student', as: :deleteproject
   resources :reports do
     collection do
-    get :monthly
-    get :weekly
-    get :daily
-    get :show_monthly
-    get :show_weekly
-    get :show_daily
-    
-  end
+      get :monthly
+      get :weekly
+      get :daily
+      get :show_monthly
+      get :show_weekly
+      get :show_daily
+    end
   end
   resources :home do
     collection do
@@ -18,12 +18,21 @@ Rails.application.routes.draw do
       get :weekly
       get :daily
       
-      
-    end
+      post :send_daily
+      end
   end
   resources :projects
   resources :recipients
-  devise_for :users 
-    resources :users
-   
+  devise_for :users
+  resources :users
+  get 'settings/profile', to: 'settings#profile', as: :settings_profile
+  post 'settings/profile', to: 'settings#update', as: :update_settings_profile
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
