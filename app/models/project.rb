26d89034f02f86b4b6055807043eed89da25597f
carlcabiles9 <cabiles.carl@yahoo.com>
2020-07-08 22:48:34 +0800

    Add has_many association for projects to report

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
+  validates :name, presence: true
   belongs_to :user, class_name: 'User'
+  has_many :reports, dependent: :destroy
   has_many :project_users, class_name: 'ProjectUser', dependent: :destroy
   has_many :participants, through: :project_users, source: :user
   searchable do
