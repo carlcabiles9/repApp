4de9f5758ee0715b408f2d46a8f20b58cd59c72c
carlcabiles9 <cabiles.carl@yahoo.com>
2020-08-13 19:13:57 +0800

    Added chosenJS

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,8 +1,8 @@
 //= require jquery
-//= require jquery_ujs
-//= require popper
-//= require bootstrap-sprockets
-//= require projects
-
+//=require jquery_ujs
+//=require popper
+//=require bootstrap-sprockets
+//=require chosen-jquery
+//= require scaffold
 require("@rails/ujs").start()
 require("turbolinks").start()
--- app/assets/javascript/projects.js.coffee
+++
@@ -1,9 +0,0 @@
-jQuery ->
-  $('#projects').dataTable
-    sPaginationType: "full_numbers"
-    bJQueryUI: true
-    bProcessing: true
-    bServerSide: true
-    sAjaxSource: $('#projects').data('source')
-
-    
