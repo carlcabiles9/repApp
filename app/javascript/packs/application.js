56307125229bc73151182bf564d97641b43b8c24
carlcabiles9 <cabiles.carl@yahoo.com>
2020-08-13 19:18:05 +0800

    Implemented Datatables

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//require chosen
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('datatables.net-bs4')
 
+import $ from 'jquery';
+global.$ = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,34 +18,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import $ from 'jquery';
-import 'bootstrap/dist/js/bootstrap';
 
 $(document).on('turbolinks:load', function() {
-  $('body').tooltip({
-    selector: '[data-toggle="tooltip"]',
-    container: 'body',
-  });
+  $('#example').DataTable();
+  $('table.display').DataTable();
 
-  $('body').popover({
-    selector: '[data-toggle="popover"]',
-    container: 'body',
-    html: true,
-    trigger: 'hover',
-  });
-  $(".chosen-select").chosen();
-  $('#example-table').DataTable();
-
-});
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ]
-  });
-
-  calendar.render();
 });
